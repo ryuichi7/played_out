df3d07f59395eabdc975606169bb1282f46ca6f2
Brett <brettrheenan@gmail.com>
2016-07-04 19:20:49 +0200

    added namespaced items resource to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
 
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :items
+    end
+  end
+
   root 'application#angular'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

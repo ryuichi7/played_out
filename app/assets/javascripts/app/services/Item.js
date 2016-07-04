76a1f4819b1877631e86f609a47d07c3e062b26c
Brett <brettrheenan@gmail.com>
2016-07-04 19:16:43 +0200

    added Item factory

---
+++ app/assets/javascripts/app/services/Item.js
@@ -0,0 +1,12 @@
+function Item($resource) {
+  var Item = $resource('/api/v1/items/:id', { id: '@id' }, {
+    update: {
+      method: 'PUT'
+    }
+  });
+
+  return Item
+}
+
+playedOut
+  .factory('Item', Item)
